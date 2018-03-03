9f7e76c2b87d8766361f449c2db6e8251eb835c3
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-03-03 16:12:32 +0100

    :wrench: Added servo config

--- src/config.js
+++ src/config.js
@@ -3,6 +3,17 @@ import styles from 'styles/core.css';
 const Config = {
   staticFolder: 'public',
   httpPort: 3000,
+  servo: {
+    pin: 'GPIO13',
+    pos: {
+      right:  0.03125, // Hardcoded value found by testing
+      left: 0.11875, // Hardcoded value found by testing
+      middle: 0.0750, // Hardcoded value found by testing
+      off: 0,
+    },
+    step: 0.00625,
+    delay: 150,
+  },
   twemoji: {
     folder: 'svg',
     ext: '.svg',
