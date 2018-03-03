4aef4298ccc14122abe3049117fe0337e9f6f84c
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-03-03 13:34:30 +0100

    :sparkles: Print logs form socket server

--- src/components/Logs/index.jsx
+++ src/components/Logs/index.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import type { Node } from 'react';
 import styles from './styles.css';
 import Log from './Log';
+import SocketIO from 'socket.io-client';
 
 type State = {
   logs: Array<{
@@ -13,25 +14,38 @@ type State = {
 
 class Logs extends Component<{}, State>
 {
+  /**
+   * Constructor
+   */
   constructor() {
     super(...arguments);
 
     this.state = {
-      logs: [
-        {
-          status: 'info',
-          text: 'Hello World',
-        },
-        {
-          status: 'fail',
-          text: 'Hello World',
-        },
-        {
-          status: 'ok',
-          text: 'Hello World',
-        },
-      ],
+      logs: [],
     };
+  }
+
+  /**
+   * Component did mount
+   */
+  componentDidMount() {
+    const socket = SocketIO(`${location.protocol}//${location.host}`);
+    socket.on('log', this.handleNewLogEvent);
+  }
+
+  /**
+   * New log event came in
+   *
+   * @param {string} log
+   */
+  handleNewLogEvent = (log: string) => {
+    const logs = this.state.logs.slice();
+
+    this.setState({
+      logs: [...logs, { status: 'info', text: log }],
+    });
+
+    this.refs.logs.scrollTop = this.refs.logs.scrollHeight;
   }
 
   /**
@@ -43,7 +57,7 @@ class Logs extends Component<{}, State>
     const { logs } = this.state;
 
     return (
-      <div className={styles.logs}>
+      <div className={styles.logs} ref='logs'>
         {logs.map((log) => {
           return (<Log status={log.status} text={log.text} key={`log-${logs.indexOf(log)}`} />);
         })}
