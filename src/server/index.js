0ddd30831ff8d749f47b6df33eb3322cd3ed52ce
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-03-03 11:44:52 +0100

    :zap: Combie socket.io server and express server in 1 http server

--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,26 @@
 //@flow
-import express from 'express';
+import Express from 'express';
+import { Server as HttpServer } from 'http';
+import SocketIO from 'socket.io';
 import { staticFolder, httpPort } from 'config';
 
-class Server extends express
+class Server
 {
   constructor() {
-    super(...arguments);
+    // Create Express app
+    const express = new Express();
 
     // Set static folder
-    this.use(express.static(staticFolder));
+    express.use(Express.static(staticFolder));
+
+    // Create http server with Express instance
+    const server = HttpServer(express);
 
     // Start server & listen on port
-    this.listen(httpPort);
+    server.listen(httpPort);
+
+    // Initialize Socket.io with http server instance
+    const io = SocketIO(server);
   }
 }
 
