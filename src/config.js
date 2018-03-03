042cd21ba6ca35954b43ab86e281b8375447259e
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-03-03 16:10:30 +0100

    :art: Moved some things

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 import styles from 'styles/core.css';
 
 const Config = {
+  staticFolder: 'public',
+  httpPort: 3000,
   twemoji: {
     folder: 'svg',
     ext: '.svg',
     className: styles.emoji,
   },
-  staticFolder: 'public',
-  httpPort: 3000,
 };
 
 module.exports = Config;
