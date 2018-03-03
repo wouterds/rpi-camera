3ea5c356103ae5f245c9fcc6b79b1385cdb8a25b
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-03-03 11:34:34 +0100

    :sparkles: Express server

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,17 @@
 //@flow
+import express from 'express';
+import { staticFolder, httpPort } from 'config';
 
-class Server
+class Server extends express
 {
   constructor() {
-    console.log('Hello World');
+    super(...arguments);
+
+    // Set static folder
+    this.use(express.static(staticFolder));
+
+    // Start server & listen on port
+    this.listen(httpPort);
   }
 }
 
