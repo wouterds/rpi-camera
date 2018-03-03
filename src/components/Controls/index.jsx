acc46607ac462a829ce63c475fcee2896823c1c8
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-03-03 16:49:59 +0100

    :sparkles: Make ajax call on click

--- src/components/Controls/index.jsx
+++ src/components/Controls/index.jsx
@@ -3,9 +3,19 @@ import React, { Component } from 'react';
 import type { Node } from 'react';
 import styles from './styles.css';
 import Button from 'components/Button';
+import axios from 'axios';
 
 class Controls extends Component<{}>
 {
+  /**
+   * Button on click handler
+   *
+   * @param {string} position
+   */
+  buttonOnClickHandler(position: string) {
+    axios.get(`/servo/${position}`);
+  }
+
   /**
    * Render the component
    *
@@ -16,10 +26,10 @@ class Controls extends Component<{}>
       <div className={styles.controls}>
         <div className={styles.buttons}>
           <div className={styles.buttonWrapper}>
-            <Button text='Left' className={styles.button} />
+            <Button text='Left' className={styles.button} onClick={() => this.buttonOnClickHandler('left')} />
           </div>
           <div className={styles.buttonWrapper}>
-            <Button text='Right' className={styles.button} />
+            <Button text='Right' className={styles.button} onClick={() => this.buttonOnClickHandler('right')} />
           </div>
         </div>
       </div>
